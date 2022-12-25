f70cc044a9073fa94b8e73f8b29da4298c312d77
Himangshu-101 <himangshu21_ug@ece.nits.ac.in>
2022-12-26 01:05:43 +0530

    Initial page Commit

---
+++ src/components/TeamHeadPhoto/TeamHeadPhoto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import main from '../../Assets/main.png'
+
+const TeamHeadPhoto = () => {
+    return(
+        <div class="slider">
+            <img src={main} alt="Head"/>
+        </div>
+    )
+}
+
+export default TeamHeadPhoto
