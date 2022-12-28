4b5f1a349bdd0dfd6d33dfa5a46f0fa00d6a352a
Partha_Pratim_Deka <Parthapratimdeka129@gmail.com>
2022-12-28 18:42:48 +0530

    ADD: Nox page

--- src/pages/Nox/Nox.jsx
+++ src/pages/Nox/Nox.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import  './Nox.css'
 
 const Nox = () => {
   return (
     <div>
-      Nox
-    </div>
+
+        <div class="first"></div>
+
+        <h2>Noixorgo</h2>
+        <h3>A weekly feed on nature </h3>
+        <p class="para"> Noixorgo is a weekly segment released by the Eco Club every sunday, which gives an
+            insight on different environmental matters that have transpired in that week.
+            This is one of the oldest segments of the club and has been running for almost two years;
+            educating and creating environmental awarness among it's readers.
+            A group of dedicated members from this club compile a set of fun and informative articles every week to
+            captivate viewers, compelling them to know more of what is happening around them.
+            This segment not only aims to promote environmental awarness, but also hopes to bring about an interest
+            among the people and allure them to know more about our Mother Earth.</p>
+        <p class="cs"><strong> Coming Soon . . .</strong> </p>
+        </div>
+
+    // </div>
   )
 }
 
