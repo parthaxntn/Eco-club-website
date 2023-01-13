f62edb9e1e98124cb1ff40328e6bc28575907a55
Himangshu-101 <himangshu21_ug@ece.nits.ac.in>
2023-01-13 22:08:12 +0530

    responsive issues resolved

--- src/components/Team/TeamLeft.jsx
+++ src/components/Team/TeamLeft.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const TeamLeft = (props) => {
     return (
-        <div className='d-flex'>
+        <div className='container1'>
                 <div className="container1-left">
                     <h3 className="from">{props.from}</h3>
                     <img className="img1" src={props.image} alt="#" />
                     <h3 className="from">{props.name}</h3>
-                    <h4 id="designation1">{props.designation}</h4>
+                    <h4 id="designation1" style={{"fontSize": "1.2rem"}}>{props.designation}</h4>
                 </div>
                 <div className="container1-right" style={{ "fontWeight": '500' }}>
                     {props.description}
