3e1fa541f36cd7d55b84ad3b133fc46e5b8ea353
Himangshu-101 <himangshu21_ug@ece.nits.ac.in>
2023-07-08 22:00:46 +0530

    Team page minor changes

--- src/components/Team/TeamWords/TeamRight.jsx
+++ src/components/Team/TeamWords/TeamRight.jsx
@@ -2,10 +2,7 @@ const TeamRight = (props) => {
     return (
         <div className="container2">
             <div className="container2-left" style={{ "fontWeight": '500' }}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis perspiciatis, soluta, quasi quam ut
-                itaque cum tempore possimus vitae minima eligendi impedit doloribus quae quos laudantium. Libero commodi
-                consequuntur sequi, a ipsa suscipit quam quibusdam? Tempora aliquam repellendus eligendi tenetur
-                provident, nisi aliquid recusandae suscipit, minima consectetur, ipsa reiciendis voluptates!
+                {props.description}
             </div>
             <div className="container2-right">
                 <h3 className="from">From Dean SW's Desk</h3>
