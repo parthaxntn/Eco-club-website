c8d24bf4fd8762a842780055caf6ab3e7776dbc8
Aryadeep Gogoi <100404453+Alsi-Aryadeep31@users.noreply.github.com>
2023-07-08 22:09:13 +0530

    noixorgo  page updates

--- src/pages/Nox/Nox.jsx
+++ src/pages/Nox/Nox.jsx
@@ -5,18 +5,7 @@ import PictureCard from './PictureCard'
 const Nox = () => {
   return (
     <div className='nox text-center'>
-      {/* <h2 className="Noix">Noixorgo</h2>
-        <h3 className="Week">A weekly feed on nature </h3>
-        <p className="para"> Noixorgo is a weekly segment released by the Eco Club every sunday, which gives an
-            insight on different environmental matters that have transpired in that week.
-            This is one of the oldest segments of the club and has been running for almost two years;
-            educating and creating environmental awarness among it's readers.
-            A group of dedicated members from this club compile a set of fun and informative articles every week to
-            captivate viewers, compelling them to know more of what is happening around them.
-            This segment not only aims to promote environmental awarness, but also hopes to bring about an interest
-            among the people and allure them to know more about our Mother Earth.</p>
-        <p className="cs"><strong> Coming Soon . . .</strong> </p>
-        </div> */}
+       
 
       <>
         <h2 className="Noix">Noixorgo</h2>
