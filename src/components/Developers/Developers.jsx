57aeaed1c0ffd6bd2de0202f2d2c324ed223d38c
shree-pragya <you@example.com>
2023-07-12 22:33:51 +0530

    Update Developers component and CSS styling

--- src/components/Developers/Developers.jsx
+++ src/components/Developers/Developers.jsx
@@ -4,26 +4,15 @@ import "./Developers.css";
 import Eighteene from "./Eighteene.js";
 
 const Developers = () => {
-  const year = ["2018-2019"];
-  const [active, setActive] = useState(year[0]);
+  const [active, setActive] = useState(null);
 
   return (
     <div className="developers-container">
       <h3 className="title">Developers</h3>
-      <div className="activeYear">
-        {year.map((item) => (
-          <div
-            key={item}
-            onClick={() => setActive(item)}
-            className={`${active === item && "active"}`}
-          >
-            {item}
-          </div>
-        ))}
-      </div>
       <div className="dev-list">
-        {active === year[0] &&
-          Eighteene.map((data, key) => <DevelopingCalling data={data} key={key} />)}
+        {Eighteene.map((data, key) => (
+          <DevelopingCalling data={data} key={key} />
+        ))}
       </div>
     </div>
   );
